275a34d4cfaed280cb4392b243ac365fd1e22e9c
Murali <murali.nutalapatii@gmail.com>
2021-08-12 07:26:50 +0100

    initial commit

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -8,7 +8,7 @@ function Counter({initialCount = 0, step = 1}) {
   // 💰 React.useReducer(countReducer, initialCount)
   const [count, setCount] = React.useState(initialCount)
 
-  // 💰 you can write the countReducer function so you don't have to make any
+  // 💰 you can write the countReducer function so you don't have to make any /
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
